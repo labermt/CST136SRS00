63cb9e21d485610b2869597b60f2bb8fb9ad8e32
Froenn <35356784+Froenn@users.noreply.github.com>
2018-04-07 10:51:11 -0700

    WIP

--- MontyHall/LetsMakeADeal.cpp
+++ MontyHall/LetsMakeADeal.cpp
@@ -1,2 +1,19 @@
 ﻿#include "stdafx.h"
 #include "LetsMakeADeal.h"
+
+LetsMakeADeal::LetsMakeADeal(const int doors, const int revealed, const int strat):
+	doors_{doors}, revealed_(revealed), strat_(strat)
+{
+	
+}
+
+LetsMakeADeal::~LetsMakeADeal()
+{
+	
+}
+
+void LetsMakeADeal::strat(int strat) const
+{
+	
+}
+
--- MontyHall/LetsMakeADeal.h
+++ MontyHall/LetsMakeADeal.h
@@ -1,7 +1,33 @@
-﻿#pragma once
+﻿#ifndef LETSMAKEADEAL_H
+#define LETSMAKEADEAL_H
 
 class LetsMakeADeal
 {
+private:
+
+	int doors_{ 3 };
+	int revealed_{ 1 };
+	int strat_;
+
+public:
+
+	enum strategy { keep = 0, trade = 1 };
+
 public:
 	
+	LetsMakeADeal(const int doors, const int revealed, const int strat);
+	~LetsMakeADeal();
+
+	void strat(int strat) const;
+	void probability();
 };
+
+#endif
+
+//create doors 
+//random pick door that hides car (1)
+//set all other doors to goat (0)
+//random pick initial door
+//apply strat
+//calculate probability and add to probability total
+//
