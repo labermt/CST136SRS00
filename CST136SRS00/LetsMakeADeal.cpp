9775ebf68549c78161c5f185a4b352b13f9ae4fb
Beau Bell <beauski00@live.com>
2018-04-07 09:20:59 -0700

    logic for parameters

---
+++ CST136SRS00/LetsMakeADeal.cpp
@@ -0,0 +1,5 @@
+#include "stdafx.h"
+#include "LetsMakeADeal.h"
+
+LetsMakeADeal::LetsMakeADeal(int door, int open, int times) : doors_{door}, disclosed_{open}, instance_{times}
+{ }
--- CST136SRS00/LetsMakeADeal.h
+++ CST136SRS00/LetsMakeADeal.h
@@ -6,8 +6,10 @@ private:
 	int doors_{ 3 };
 	int disclosed_{ 1 };
 	int instance_{};
+	bool strat_{};
 
 public:
-	
+	LetsMakeADeal(int door, int open, int times);
+
 
 };
