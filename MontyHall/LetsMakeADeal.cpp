650c1b626dac6df467a72979d7cf11d2bbd711d3
Seadra <colin.rexing@gmail.com>
2018-04-09 15:38:15 -0700

    getting further, builds but no tests have been ran yet

--- MontyHall/LetsMakeADeal.cpp
+++ MontyHall/LetsMakeADeal.cpp
@@ -4,14 +4,32 @@
 #include <random>
 
 
-LetsMakeADeal::LetsMakeADeal(unsigned doors, unsigned open_doors, std::string strat) :
-	doors_{ doors }, open_doors_{ open_doors }, strat_{strat}
+unsigned LetsMakeADeal::get_random_door_index(unsigned const max_door) const
 {
+	auto result{0};
+	// generate random number
+	//assign it to result
+	//while value @ result == car repeat steps
+	//return result
+	return result;
+}
+
+void LetsMakeADeal::set_prize(std::vector<unsigned> game_instance, unsigned location)
+{
+	
+}
+
+LetsMakeADeal::LetsMakeADeal(unsigned doors, unsigned open_doors, std::string strat) :
+	doors_{ doors }, open_doors_{ open_doors }
+{
+	if (strat.compare("stay")) {strat_ = stay;}
+	else {strat_ = change;}
 }
 
 bool LetsMakeADeal::run_game()
 {
-	std::vector<unsigned> total_doors{doors_};
+	std::vector<unsigned> game_instance{doors_, goat};
+	set_prize(game_instance, get_random_door_index(doors_));
 	// get random number between 0 and total doors
 	//
 	auto result = false;
--- MontyHall/LetsMakeADeal.h
+++ MontyHall/LetsMakeADeal.h
@@ -5,9 +5,15 @@ class LetsMakeADeal
 {
 
 private:
-	unsigned doors_{3};
-	unsigned open_doors_{1};
-	std::string strat_{};
+	enum strategy {stay, change};
+	enum prize{goat, car};
+	
+	unsigned doors_{};
+	unsigned open_doors_{};
+	strategy strat_{};
+
+	unsigned get_random_door_index(unsigned const max_door) const;
+	void set_prize(std::vector<unsigned> game_instance, unsigned location);
 
 public:
 	LetsMakeADeal(unsigned doors, unsigned open_doors, std::string strat);
