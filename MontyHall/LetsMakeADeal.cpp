92ae0da9916e722c65cac625c6554821a61a2ec8
Seadra <colin.rexing@gmail.com>
2018-04-09 12:22:34 -0700

    better start, still unsolved issues
    All test code, none has been ran

--- MontyHall/LetsMakeADeal.cpp
+++ MontyHall/LetsMakeADeal.cpp
@@ -4,18 +4,14 @@
 #include <random>
 
 
-LetsMakeADeal::LetsMakeADeal(unsigned doors, unsigned open_doors) :
-	doors_{ doors },
-	open_doors_{ open_doors },
-	length_{ doors_ - open_doors_ },
-	total_doors_{length_,0}
+LetsMakeADeal::LetsMakeADeal(unsigned doors, unsigned open_doors, std::string strat) :
+	doors_{ doors }, open_doors_{ open_doors }, strat_{strat}
 {
-	std::uniform_int_distribution<int> car(0, length_);
-	// set index @ random number generated car = 1 representing the car
 }
 
 bool LetsMakeADeal::run_game()
 {
+	std::vector<unsigned> total_doors{doors_};
 	// get random number between 0 and total doors
 	//
 	auto result = false;
--- MontyHall/LetsMakeADeal.h
+++ MontyHall/LetsMakeADeal.h
@@ -7,12 +7,9 @@ class LetsMakeADeal
 private:
 	unsigned doors_{3};
 	unsigned open_doors_{1};
-	unsigned length_{};
-	std::vector<unsigned> total_doors_{};
-	
+	std::string strat_{};
 
 public:
-	LetsMakeADeal(unsigned doors, unsigned open_doors);
+	LetsMakeADeal(unsigned doors, unsigned open_doors, std::string strat);
 	bool run_game();
 };
-
