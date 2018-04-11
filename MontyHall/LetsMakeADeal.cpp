c1efec51aaf3431d55f308feaecf176a8e84b7d4
an6688 <anil.coklar@oit.edu>
2018-04-10 20:55:58 -0700

    some of the code done
    yerp

--- MontyHall/LetsMakeADeal.cpp
+++ MontyHall/LetsMakeADeal.cpp
@@ -1,14 +1,32 @@
 #include "stdafx.h"
 #include "LetsMakeADeal.h"
+#include <random>
 
 
-LetsMakeADeal::LetsMakeADeal(size_t numOfDoors_, size_t openDoors_, size_t finalLength_)
+LetsMakeADeal::LetsMakeADeal(size_t numOfDoors, size_t openDoors) :
+	numOfDoors_{ numOfDoors },
+	openDoors_{ openDoors },
+	finalLength_{ numOfDoors - openDoors },
+	chosenDoor_{}
 {
-	finalLength_ = numOfDoors_ - openDoors_;
-
+	for (size_t i{}; i < finalLength_ ;i++)
+	{
+		doors_[i] = behindTheDoor::goat;//this puts a goat in each door
+	}
+	doors_[randomDoor()] = behindTheDoor::car;
 }
-
-
-//LetsMakeADeal::~LetsMakeADeal()
-//{
-//}
+int LetsMakeADeal::randomDoor() const
+{	//need to fix randomizer 
+	//taken from http://en.cppreference.com/w/cpp/numeric/random/uniform_int_distribution
+	/*std::default_random_engine generator;
+	std::uniform_int_distribution<int> distribution();*/
+	//static std::random_device rd;  //Will be used to obtain a seed for the random number engine
+	//static std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+	//const std::uniform_int_distribution<size_t> dis(0, finalLength_ - 1);
+	//return dis(gen);
+}
+::LetsMakeADeal::behindTheDoor LetsMakeADeal::makeDeal(bool stay)
+{
+	auto result(behindTheDoor::goat); 
+	return result;
+}
--- MontyHall/LetsMakeADeal.h
+++ MontyHall/LetsMakeADeal.h
@@ -1,13 +1,19 @@
 #pragma once
+#include <vector>
 class LetsMakeADeal
 {
-private: 
-	size_t numOfDoors_{ 3 }; //start amount of doors
-	size_t openDoors_{ 1 }; //how many doors to open or predisclose 
-	size_t finalLength_; //how many doors you wind up with
-	bool stay = true;  //switch or stay doors 
 public:
-	LetsMakeADeal(size_t numOfDoors_, size_t openDoors_, size_t finalLength_);
-	
+	LetsMakeADeal(size_t numOfDoors, size_t openDoors);
+	enum class behindTheDoor {goat, car};
+	behindTheDoor makeDeal(bool stay); 
+	int randomDoor() const;
+private: 
+	const size_t numOfDoors_{}; //start amount of doors
+	const size_t openDoors_{}; //how many doors to open or predisclose 
+	const size_t finalLength_; //how many doors you wind up with
+	bool stay_ = true;  //switch or stay doors, run at rungame
+	size_t chosenDoor_; 
+	std::vector<behindTheDoor> doors_{finalLength_}; 
+	//size_t finalLength
 };
 
