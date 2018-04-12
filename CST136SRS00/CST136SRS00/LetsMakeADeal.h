f6236f755a3ff8bd5fd895373b167411d380d9a2
timothy carpenter <tj.carpenter@gmail.com>
2018-04-12 07:52:13 -0700

    updates to class

--- CST136SRS00/CST136SRS00/LetsMakeADeal.h
+++ CST136SRS00/CST136SRS00/LetsMakeADeal.h
@@ -1,14 +1,23 @@
 #pragma once
 class LetsMakeADeal
 {
-
-
-
+	
 public:
 	LetsMakeADeal();
 	virtual ~LetsMakeADeal();
 
+	LetsMakeADeal(const int total_doors, const int open_doors, const bool switch_strategy, const float win_probability)
+		: totalDoors(total_doors),
+		  openDoors(open_doors),
+		  switchStrategy(switch_strategy),
+		  winProbability(win_probability)
+	{
+	}
 
-
+private:
+	int totalDoors;
+	int openDoors;
+	bool switchStrategy;
+	float winProbability;
 };
 
