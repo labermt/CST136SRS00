f418c770a1a0fc24c96ae6192e31d60f53b9e167
an6688 <anil.coklar@oit.edu>
2018-04-14 09:13:08 -0700

    playgame kind of works

--- MontyHall/LetsMakeADeal.cpp
+++ MontyHall/LetsMakeADeal.cpp
@@ -12,9 +12,9 @@ LetsMakeADeal::LetsMakeADeal(size_t numOfDoors, size_t openDoors, const Strategy
 {
 	for (size_t i{}; i < finalLength_;i++)
 	{
-		doors_[i] = behindTheDoor::goat;// this puts a goat in each door
+		doors_[i] = prize::goat;// this puts a goat in each door
 	}
-	doors_[randomDoor()] = behindTheDoor::car;
+	doors_[randomDoor()] = prize::car;
 }
 
 int LetsMakeADeal::randomDoor() const
@@ -26,11 +26,46 @@ int LetsMakeADeal::randomDoor() const
 	return dis(gen);
 }
 
-LetsMakeADeal::behindTheDoor LetsMakeADeal::playGame()
+LetsMakeADeal::prize LetsMakeADeal::playGame()
 {
+	// choose a door at random
+	doors_[randomDoor()] = prize::car;
+	guessDoor(); 
+	// disclose the doors that does not have the car or the chosen door 
+	openDoors();
+	// if switch, then choose an available door (cannot be disclosed)
+	if (strategy_== Strategy::kSwitch);
+	{
+		guessDoor(); 
+	}
+	
+	// open door, return prize 
 
-	auto result(behindTheDoor::goat);
-	return result;
+
 }
 
+void LetsMakeADeal::guessDoor()
+{
+	auto guess = randomDoor(); 
+	while (doors_[guess] == prize::opened || guess_ == guess)
+	{
+		guess = randomDoor();
+	}
+	guess_ = guess; 
+}
+
+void LetsMakeADeal::openDoors()
+{
+	auto guess = randomDoor(); 
+	auto doorTally{ 0 };
+	while (doorTally < openDoors_)
+	{
+		while (doors_[guess] == prize::opened || guess_ == guess)
+		{
+			guess = randomDoor();
+		}
+		doors_[guess] = prize::opened;
+		doorTally++; 
+	}
+}
 
--- MontyHall/LetsMakeADeal.h
+++ MontyHall/LetsMakeADeal.h
@@ -2,21 +2,26 @@
 #include <vector>
 class LetsMakeADeal
 {
+
 public:
 	enum class Strategy { kUnknown, kSwitch, kStay }; // 'k' lets you use switch 
 	int randomDoor() const;
 	LetsMakeADeal(size_t numOfDoors, size_t openDoors, Strategy strategy);
-	enum class behindTheDoor {goat, car};
-	behindTheDoor playGame(); 
+	enum class prize {goat, car, opened};
+	prize playGame(); 
+
 
 private: 
-	bool stay_ = true;  //switch or stay doors, run at plaay game
+	bool stay_ = true;  // switch or stay doors, run at plaay game
 	size_t chosenDoor_{};
-	const size_t numOfDoors_{}; //start amount of doors
-	const size_t openDoors_{}; //how many doors to open or predisclose 
-	const size_t finalLength_; //how many doors you wind up with
+	const size_t numOfDoors_{}; // start amount of doors
+	const size_t openDoors_{}; // how many doors to open or predisclose 
+	const size_t finalLength_; // how many doors you wind up with
 	const Strategy strategy_; 
+	int guess_; 
+	void guessDoor(); 
+	void openDoors(); 
+	std::vector<prize> doors_{finalLength_}; 
 
-	std::vector<behindTheDoor> doors_{finalLength_}; 
 };
 
